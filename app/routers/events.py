116706f4254ab7d6a08e688ed5cb9a3902297c89
Tuuli <tuulimarj@hotmail.com>
2023-04-30 22:31:48 +0300

    minor fixes

--- app/routers/events.py
+++ app/routers/events.py
@@ -27,11 +27,11 @@ def get_events(player_id: int, db: Session = Depends(get_db)):
     return crud_events.fetch_events(db, player_id)
 
 #ei toimi vielä
-@router.get('/with_types')
-def get_with_type(type: str = '', db: Session = Depends(get_db)):
-    if type != '':
-        pass
-    return crud_events.fetch_with_type(db)
+# @router.get('/with_types')
+# def get_with_type(type: str = '', db: Session = Depends(get_db)):
+#     if type != '':
+#         pass
+#     return crud_events.fetch_with_type(db)
 
 #####POST
 
